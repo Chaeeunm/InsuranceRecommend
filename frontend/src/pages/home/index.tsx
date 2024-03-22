e9b00d81ec8deaa3e0db6016402374378db5917a
DongjaJ <jdh981118@naver.com>
2024-03-22 11:56:45 +0900

    chore: shadcn-ui 재설치

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,10 +1,9 @@
 import { Button } from '@/components/ui/button';
 
-const Home = () => (
-  <div>
-    홈 페이지
-    <Button>Click me</Button>
-  </div>
-);
-
-export default Home;
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
