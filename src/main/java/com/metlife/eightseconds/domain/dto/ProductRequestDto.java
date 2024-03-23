f96a1916bbe9f430b9cb038257d952b924206009
xoghkscc <xoghkscc@naver.com>
2024-03-23 19:11:45 +0000

    Updated ProductRequestDto.java

--- src/main/java/com/metlife/eightseconds/domain/dto/ProductRequestDto.java
+++ src/main/java/com/metlife/eightseconds/domain/dto/ProductRequestDto.java
@@ -2,9 +2,8 @@ package com.metlife.eightseconds.domain.dto;
 
 import java.util.List;
 
-import com.metlife.eightseconds.domain.data.AIJsonResponse;
 import com.metlife.eightseconds.domain.enums.PaymentType;
-
+import com.metlife.eightseconds.domain.enums.RiskType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,25 +12,29 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductRequestDto {
+public class ProductResponseDto {
 
-	private Integer guaranteePeriod;
-	private Integer productPrice;
-	private Integer paymentPeriod;
-	private Boolean renewYn;    // 갱신여부
+	private Integer productId; //상품아이디
+	private String productName; //상품명
+	private Integer productPrice; //보혐료
+	private String productDate; //보험기간
+	private Integer productExp; //보장범위지수
+	private Integer minAge; //최소가입연령
+	private Integer maxAge; //최대가입연령
+	private String productEtc; //비고
+	private String productType; //비고
+	private String age; //비고
+	private String aiText; //비고
+	private List<CoveredResponseDto> coverResponseList; //보장리스트
+	private Integer paymentMinPeriod;
+	private Integer paymentMaxPeriod;
+	private Boolean renewYn;
 	private PaymentType paymentType;
-	private List<String> coverDisease;
-
-	@Builder
-	public ProductRequestDto(AIJsonResponse response){
-		this.productPrice = response.getProductPrice();
-		this.guaranteePeriod = response.getGuaranteePeriod();
-		this.paymentPeriod = response.getPaymentPeriod();
-		this.renewYn = (response.getRenewYn().equals("1") ? false : true);
-		this.paymentType = (response.getPaymentType().equals("1") ? PaymentType.ALL : PaymentType.ANNUAL);
-		this.coverDisease = response.getCoverDisease();
-	}
+	private Double interestRate;
+	private RiskType dangerRank;
+	private String aiResponse;
 
 }
