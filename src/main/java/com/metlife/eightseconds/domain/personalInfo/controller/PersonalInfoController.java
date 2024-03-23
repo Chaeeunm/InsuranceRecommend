9fa0bd68f45800ef9b8d721cd6974c304e42f8da
Chaeeunm <rlacodms0525@naver.com>
2024-03-23 19:34:34 +0900

    Feat: personalInfo 입력

---
+++ src/main/java/com/metlife/eightseconds/domain/personalInfo/controller/PersonalInfoController.java
@@ -0,0 +1,12 @@
+package com.metlife.eightseconds.domain.personalInfo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class PersonalInfoController {
+
+
+}
---
+++ src/main/java/com/metlife/eightseconds/domain/personalInfo/enums/Gender.java
@@ -0,0 +1,12 @@
+package com.metlife.eightseconds.domain.personalInfo.enums;
+
+public enum Gender {
+	MALE("남성"),
+	FEMALE("여성");
+
+	private final String value;
+
+	private Gender(String value){
+		this.value = value;
+	}
+}
