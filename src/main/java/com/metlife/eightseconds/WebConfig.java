283760b9c879184cbd4d860d1cd61dab29c66fee
xoghkscc <xoghkscc@naver.com>
2024-03-23 19:48:30 +0000

    Added WebConfig.java

---
+++ src/main/java/com/metlife/eightseconds/WebConfig.java
@@ -0,0 +1,12 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type")
+                .exposedHeaders("Custom-Header")
+    }
+}
