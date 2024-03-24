e9f87436fc1b5b37e4f3aeefc021d4b8077debf5
Chaeeunm <rlacodms0525@naver.com>
2024-03-24 09:35:23 +0900

    initial commit

--- src/main/java/com/metlife/eightseconds/WebConfig.java
+++ src/main/java/com/metlife/eightseconds/WebConfig.java
@@ -1,12 +1,18 @@
+package com.metlife.eightseconds;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Content-Type")
-                .exposedHeaders("Custom-Header");
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type")
+            .exposedHeaders("Custom-Header");
     }
 }
